b6d857f2bf59977e28b3a1655669bd5c5284facd
Lookas123 <42605685+Lookas123@users.noreply.github.com>
2021-06-26 15:48:31 +0300

    accurate
    Added an outcome column at the end, made poolings work correctly, and fixed df/dh chances

--- GCcalc.js
+++ GCcalc.js
@@ -18,9 +18,13 @@ let emg = emgtoggle.checked;
 let dfactive = dfactivetoggle.checked;
 let skruuia = skruuiatoggle.checked;
 let gpocstage = gpocinput.value;
-let chances = [1,1,cbt?0.03:0,cbt?0.03:0,dfactive?0.005:0.1,bs?0.25:0,0.0005,0.0001,dh?0.1925:(rb?0.01925:0),df?0.1925:(rb?0.01925:0),emg?0.05:0];
-let wchances = [1,cbt?0.321:0.3,cbt?0.321:0.3,dfactive?0.005:0.1,bs?0.25:0,0.0005,0.0001,dh?0.05:(rb?0.005:0),df?0.05:(rb?0.005:0),emg?0.05:0,1,1,0.3,0.1]
-if(skruuia) wchances.push(1,1,1,1)
+let chances = [{c:[1,1,0,0,dfactive?0.005:0.1,bs?0.25:0,0.0005,0.0001,0,0,emg?0.05:0],p:0.783275}, {c:[1,cbt?1:0,cbt?1:0,1,dfactive?0.005:0.1,bs?0.25:0,0.0005,0.0001,0,0,emg?0.05:0],p:0.024225},{c:[1,1,0,0,dfactive?0.005:0.1,bs?0.25:0,0.0005,0.0001,df?1:(rb?0.1:0),dh?1:(rb?0.1:0),emg?0.05:0],p:0.186725},{c:[1,1,cbt?1:0,cbt?1:0,dfactive?0.005:0.1,bs?0.25:0,0.0005,0.0001,df?1:(rb?0.1:0),dh?1:(rb?0.1:0),emg?0.05:0],p:0.005775}];
+let wchances = [{c:[1,0,0,dfactive?0.005:0.1,bs?0.25:0,0.0005,0.0001,0,0,emg?0.05:0,1,1,0,0.1],p:0.64505},{c:[1,1,1,dfactive?0.005:0.1,bs?0.25:0,0.0005,0.0001,0,0,emg?0.05:0,1,1,1,0.1],p:0.285}, {c:[1,cbt?1:0,cbt?1:0,dfactive?0.005:0.1,bs?0.25:0,0.0005,0.0001,0,0,emg?0.05:0,1,1,0,0.1],p:0.01995},{c:[1,0,0,dfactive?0.005:0.1,bs?0.25:0,0.0005,0.0001,df?1:(rb?0.1:0),dh?1:(rb?0.1:0),emg?0.05:0,1,1,0,0.1],p:0.03395},{c:[1,1,1,dfactive?0.005:0.1,bs?0.25:0,0.0005,0.0001,df?1:(rb?0.1:0),dh?1:(rb?0.1:0),emg?0.05:0,1,1,1,0.1],p:0.015}, {c:[1,cbt?1:0,cbt?1:0,dfactive?0.005:0.1,bs?0.25:0,0.0005,0.0001,df?1:(rb?0.1:0),dh?1:(rb?0.1:0),emg?0.05:0,1,1,0,0.1],p:0.00105}]
+if(skruuia) {
+    for (let index = 0; index < wchances.length; index++) {
+        wchances[index].c.push(1,1,1,1)
+    }
+}
 console.log(wchances);
 let dchances=[];
 let dwchances=[];
@@ -32,24 +36,26 @@ let header = "<tr><th>Outcome</th>"
 let content = ""
 for(let p = -1; p<effectnames.length; p++){    
     let output = [0,0,0,0,0,0,0,0,0,0,0]
-    if(p<chances.length){
-    for(let i=0; i<2**chances.length;i++){
-        let pool=[];
-        let n=i;
-        for(let a=0; a<chances.length;a++){
-            n-=(pool[a]=n%2);
-            n/=2;
-        }
-        let chanceforpool=1;   
-        let poolcount=0;
-        for(let a=0; a<chances.length;a++){
-            let chance = chances[a]*((p==a&&p!=7)?0.2:1);
-            chanceforpool*=pool[a]?chance:1-chance
-            poolcount+=pool[a];
-        }
-        //console.log(chanceforpool, poolcount, pool)
-        for(let a=0; a<chances.length;a++){
-            if(pool[a]) output[a]+=chanceforpool/poolcount
+    if(p<chances[0].c.length){
+    for (let index = 0; index < chances.length; index++) {
+        for(let i=0; i<2**chances[index].c.length;i++){
+            let pool=[];
+            let n=i;
+            for(let a=0; a<chances[index].c.length;a++){
+                n-=(pool[a]=n%2);
+                n/=2;
+            }
+            let chanceforpool=chances[index].p;   
+            let poolcount=0;
+            for(let a=0; a<chances[index].c.length;a++){
+                let chance = chances[index].c[a]*((p==a&&p!=7)?0.2:1);
+                chanceforpool*=pool[a]?chance:1-chance
+                poolcount+=pool[a];
+            }
+            //console.log(chanceforpool, poolcount, pool)
+            for(let a=0; a<chances[index].c.length;a++){
+                if(pool[a]) output[a]+=chanceforpool/poolcount
+            }
         }
     }
     header+="<th> Golden Cookie, Previous: " + effectnames[p] + "</th>"
@@ -62,30 +68,28 @@ else basechances=output
 
 for(let p = -1; p<effectnames.length; p++){
     let output = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    if ((p>=1&&wchances[p-1]!=undefined)||p<0){
-    for(let i=0; i<2**wchances.length;i++){
-        let pool=[];
-        let n=i;
-        for(let a=0; a<wchances.length;a++){
-            n-=(pool[a]=n%2);
-            n/=2;
+    if ((p>=1&&wchances[0].c[p-1]!=undefined)||p<0){
+        for (let index = 0; index < wchances.length; index++) {
+            for(let i=0; i<2**wchances[index].c.length;i++){
+                let pool=[];
+                let n=i;
+                for(let a=0; a<wchances[index].c.length;a++){
+                    n-=(pool[a]=n%2);
+                    n/=2;
+                }
+                let chanceforpool=wchances[index].p;   
+                let poolcount=0;
+                for(let a=0; a<wchances[index].c.length;a++){
+                    let chance = wchances[index].c[a]*((p==a&&p!=7)?0.2:1);
+                    chanceforpool*=pool[a]?chance:1-chance
+                    poolcount+=pool[a];
+                }
+                //console.log(chanceforpool, poolcount, pool)
+                for(let a=0; a<wchances[index].c.length;a++){
+                    if(pool[a]) output[a]+=chanceforpool/poolcount
+                }
+            }
         }
-        let chanceforpool=1;   
-        let poolcount=0;
-        for(let a=0; a<wchances.length;a++){
-            let chance = wchances[a]*(p-1==a&&p!=7?0.2:1);
-            chanceforpool*=pool[a]?chance:1-chance
-            poolcount+=pool[a];
-        }
-        //console.log(chanceforpool, poolcount, pool)
-        for(let a=0; a<wchances.length;a++){
-            if(pool[a]) output[a]+=chanceforpool/poolcount
-        }
-    }
-/*for(let a = 7; a>-1; a--){
-    output[a]+=output[a+4]
-    output[a+4]=undefined;
-}*/
 header+="<th> Wrath Cookie, Previous: " + effectnames[p] + "</th>"
 output.unshift(0);
 if(!skruuia) output.push(0,0,0,0)
@@ -113,12 +117,13 @@ for(let i = 0; i < effectnames.length;i++){
 }
 ebasechances=c;
 }
-header+="<th>Cumulative chances</th></tr>"
+header+="<th>Cumulative chances</th><th>Outcome</th></tr>"
 outputarray[outputarray.length]=ebasechances;
+outputarray[outputarray.length]=(effectnames)
 for (let i = 0; i < outputarray[0].length; i++) {
     content += "<tr>"
     for (let a = 0; a < outputarray.length; a++) {
-        content += "<td>"+ (a>0?Number(outputarray[a][i]).toFixed(7):outputarray[a][i])+"</td>";
+        content += "<td>"+ (a>0&&a<outputarray.length-1?Number(outputarray[a][i]).toFixed(7):outputarray[a][i])+"</td>";
     }
     content += "</tr>"
 }
